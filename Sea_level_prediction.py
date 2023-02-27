dacb0b92825f117f38667bf9cd9ac7e5762bb3de
Mr.Utop <107547010+Jubilorote@users.noreply.github.com>
2023-02-27 20:00:57 +0100

    Update Sea_level_prediction.py

--- Sea_level_prediction.py
+++ Sea_level_prediction.py
@@ -36,6 +36,5 @@ def draw_plot():
   plt.ylabel("Sea Level (inches)")
   plt.title("Rise in Sea Level")
     
-    # Save plot and return data for testing (DO NOT MODIFY)
   plt.savefig('sea_level_plot.png')
   return plt.gca()
